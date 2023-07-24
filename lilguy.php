eca6aa80c0518eb003906e5e3cf5bb0e5423859d
Wren <139609981+wren-nest@users.noreply.github.com>
2023-07-24 14:23:16 +1000

    update

--- lilguy.php
+++ lilguy.php
@@ -1,4 +1,7 @@
-<html>
+<!-- Use terminal to navigate to the right folder and run: $ php -S 0.0.0.0:8000 -->
+<!-- NB: Use the port number 8000 !! -->
+
+<!DOCTYPE html>
 <body>
 
 <?php echo $_POST["lilguy"]; ?>
